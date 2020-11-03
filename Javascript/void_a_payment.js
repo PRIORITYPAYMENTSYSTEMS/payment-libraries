976e64ac3b259a56e7b5376adbc43eb2c664e2e8
Kirsty Russell <kirsty.lou.r@gmail.com>
2020-11-03 12:17:49 -0500

    Update void_a_payment.js

--- Javascript/void_a_payment.js
+++ Javascript/void_a_payment.js
@@ -1,7 +1,7 @@
 ﻿fetch("https://sandbox.api.mxmerchant.com/checkout/v3/payment/{{paymentId}}", {
     "method": "DELETE",
     "headers": {
-        "Authorization": "Basic a2lyc3R5LnJ1c3NlbGw6TXZTbVhtUCExODc1"
+        "Authorization": "Basic dGVzdGFjY291bnQ6ZmFrZXBhc3N3b3Jk"
     }
 })
     .then(response => {
