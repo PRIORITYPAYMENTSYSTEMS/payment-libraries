b1a2b70f10cf6e81379273f00e3b2b0c8cc0184d
Kirsty Russell <kirsty.lou.r@gmail.com>
2020-11-03 12:17:36 -0500

    Update make_a_payment.js

--- Javascript/make_a_payment.js
+++ Javascript/make_a_payment.js
@@ -2,7 +2,7 @@
   "method": "POST",
   "headers": {
     "Content-Type": "application/json",
-    "Authorization": "Basic a2lyc3R5LnJ1c3NlbGw6TXZTbVhtUCExODc1"
+    "Authorization": "Basic dGVzdGFjY291bnQ6ZmFrZXBhc3N3b3Jk"
   },
   "body": "{\"paymentType\":\"Sale\",\"cardAccount\":{\"number\":\"4111111111111111\",\"expiryMonth\":\"06\",\"expiryYear\":\"23\",\"avsZip\":\"30004\",\"cvv\":\"321\"},\"entryClass\":\"WEB\",\"authOnly\":false,\"isAuth\":false,\"isSettleFunds\":true,\"source\":\"API\",\"taxExempt\":false,\"merchantId\":\"000000000\",\"amount\":0.01,\"tenderType\":\"card\",\"cardPresent\":true}"
 })
